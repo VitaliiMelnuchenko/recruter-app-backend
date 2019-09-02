7444b5a4a086d1a0a7f3a8f1beba46790300a41c
Віталій Мельниченко <melnychenko.vitaliy.1@gmail.com>
2019-09-02 15:37:29 +0300

    Add *

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
 app.use(function(req, res, next) {
-   res.header("Access-Control-Allow-Origin", "YOUR-DOMAIN.TLD");
+   res.header("Access-Control-Allow-Origin", "*");
    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
    next();
 });
