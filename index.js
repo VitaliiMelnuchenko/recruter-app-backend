e9090a9a2cb3f9f09a6fcc2c8e6ed6742c5c42a1
Віталій Мельниченко <melnychenko.vitaliy.1@gmail.com>
2019-09-02 15:07:28 +0300

    Add dynamic PORT

--- index.js
+++ index.js
@@ -23,6 +23,6 @@ app.use('*', (req, res) => {
    res.status(404).json({ message: 'Error 404: Not found' });
 });
 
-app.listen(3000, () => {
+app.listen(process.env.PORT, process.env.ID, () => {
    console.log('server is running');
 });
