511fa197d82a8a461f87a305690153266af20f2d
Віталій Мельниченко <melnychenko.vitaliy.1@gmail.com>
2019-09-02 15:25:53 +0300

    Fix

--- index.js
+++ index.js
@@ -24,8 +24,8 @@ app.use('*', (req, res) => {
 });
 
 app.use(function(req, res, next) {
-   res.header("Access-Control-Allow-Origin", "YOUR-DOMAIN.TLD"); // update to match the domain you will make the request from
-   res.header("Access-Control-Allow-Headers", "Authorization, Origin, X-Requested-With, Content-Type, Accept");
+   res.header("Access-Control-Allow-Origin", "YOUR-DOMAIN.TLD");
+   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
    next();
  });
 
